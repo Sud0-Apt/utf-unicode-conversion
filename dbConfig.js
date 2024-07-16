bdfa8f5fb1103abca3ee68b403922ed580a9be92
Skanda M Shastry <126911189+Sud0-Apt@users.noreply.github.com>
2024-07-16 16:18:08 +0530

    Add files via upload

---
+++ dbConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    originalDb: {
+        host: 'localhost',
+        user: 'username',
+        password: 'StrongPassw0rd!',
+        database: 'original_db',
+    },
+    utfDb: {
+        host: 'localhost',
+        user: 'username',
+        password: 'StrongPassw0rd!',
+        database: 'utf_db',
+    }
+};
